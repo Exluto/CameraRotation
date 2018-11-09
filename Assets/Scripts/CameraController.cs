b06836bed8647531f6f364d577a15e66c6d6e6e3
Tom Sauer <tom@exluto.com>
2018-11-08 20:38:21 -0500

    cleaning up camera script, testing coldAir

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -13,32 +13,31 @@ public class CameraController : MonoBehaviour {
     [SerializeField] float minDistance = 2f;
     [SerializeField] float maxDistance = 5f;
  
-    void Update ()
-    {
+    void Update () {
         CameraControl();
     }
  
-    void CameraControl()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            /* Rotate the camera using the input managers mouse axis */
+    void CameraControl() {
+        if (Input.GetMouseButton(0)) {
+            /// <summary>
+            /// Rotate the camera using the input managers mouse axis
+            /// </summary>
             transform.RotateAround(target.transform.position, Vector3.up, ((Input.GetAxisRaw("Mouse X") * Time.deltaTime) * moveSpeed));
             transform.RotateAround(target.transform.position, transform.right, -((Input.GetAxisRaw("Mouse Y") * Time.deltaTime) * moveSpeed));
         }
- 
-        /* Zoom the camera */
         ZoomCamera();
     }
  
-    void ZoomCamera()
-    {
-        /* If we are already close enough for the min distance and we try to zoom in, dont, return instead */
-        /* Similarly for zooming out */
+    void ZoomCamera() {
+        /// <summary>
+        /// If we are already close enough for the min distance and we try to zoom in, dont, return instead
+        /// Similarly for zooming out
+        /// </summary>
         if (Vector3.Distance(transform.position, target.transform.position) <= minDistance && Input.GetAxis("Mouse ScrollWheel") > 0f) { return; }
         if (Vector3.Distance(transform.position, target.transform.position) >= maxDistance && Input.GetAxis("Mouse ScrollWheel") < 0f) { return; }
- 
-        /* Only move in the Z relative to the Camera (so forward and back) */
+        /// <summary>
+        /// Only move in the Z relative to the Camera (so forward and back)
+        /// </summary>
         transform.Translate(
             0f,
             0f,
